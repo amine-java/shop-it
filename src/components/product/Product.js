408bce42589780f436880e0d5f79a1de706e5d91
System Administrator <root@MacBook-Pro-de-Amine.local>
2019-10-12 18:59:51 +0200

    Using state instead of props in the render method

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -4,21 +4,39 @@ import './Product.css';
 export class Product extends React.Component {
 
 
-    constructor(){
-        super();
+    state = {
+        title : this.props.title , 
+        image : this.props.image , 
+        description : this.props.description , 
+        price : this.props.price 
+    }
+
+    /**
+     * Constructor
+     * @param {*} props 
+     */
+    constructor(props){
+        super(props);
         this.onAddToCart = this.onAddToCart.bind(this);
     }
+
+    /**
+     * Render method
+     */
     render(){        
         return ( 
         <div className="card">
-            <img src={this.props.image} alt="Denim Jeans"/>
-            <h1>{this.props.title}</h1>
-            <p className="price">${this.props.price}</p>
-            <p>{this.props.description}</p>
+            <img src={this.state.image} alt="Denim Jeans"/>
+            <h1>{this.state.title}</h1>
+            <p className="price">${this.state.price}</p>
+            <p>{this.state.description}</p>
             <p><button onClick={this.onAddToCart}>Add to Cart</button></p>
           </div>);
     }
     
+    /**
+     * OnAddToCart handler event
+     */
     onAddToCart(){
         alert('You just added: ' + this.props.title + ' to your cart');
     }
